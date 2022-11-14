f5bae9c59bb96bf4390b81059d8eac447d46748e
usbbush <jeanbienvenusb@gmail.com>
2022-11-14 11:41:42 +0200

    Fix rubocop complaints

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -18,8 +18,11 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
   test 'should create recipe' do
     assert_difference('Recipe.count') do
       post recipes_url,
-           params: { recipe: { cooking_time: @recipe.cooking_time, description: @recipe.description, name: @recipe.name,
-                               preparation_time: @recipe.preparation_time, public: @recipe.public, user_id: @recipe.user_id } }
+           params: { recipe: { cooking_time: @recipe.cooking_time,
+                               description: @recipe.description,
+                               name: @recipe.name,
+                               preparation_time: @recipe.preparation_time,
+                               public: @recipe.public, user_id: @recipe.user_id } }
     end
 
     assert_redirected_to recipe_url(Recipe.last)
@@ -37,8 +40,12 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should update recipe' do
     patch recipe_url(@recipe),
-          params: { recipe: { cooking_time: @recipe.cooking_time, description: @recipe.description, name: @recipe.name,
-                              preparation_time: @recipe.preparation_time, public: @recipe.public, user_id: @recipe.user_id } }
+          params: { recipe:
+                      { cooking_time: @recipe.cooking_time,
+                        description: @recipe.description,
+                        name: @recipe.name,
+                        preparation_time: @recipe.preparation_time,
+                        public: @recipe.public, user_id: @recipe.user_id } }
     assert_redirected_to recipe_url(@recipe)
   end
 
