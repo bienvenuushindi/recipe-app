736783b1572fc3c3e5988e66692ff0caa080e1ef
usbbush <jeanbienvenusb@gmail.com>
2022-11-15 20:50:37 +0200

    Generate new schema after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_092126) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_152957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_092126) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "foods_counter", default: 0
+    t.integer "foods_total_price", default: 0
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
