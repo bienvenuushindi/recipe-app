6b8cdca3a9302b2e6166b327554598f475b019f0
tkz96 <tkz9696@gmail.com>
2022-11-15 15:49:37 +0500

    Add condition to login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
   def index
