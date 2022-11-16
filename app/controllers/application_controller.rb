13fb88c0dd5b04476d14b83d70e3619d385fb523
usbbush <jeanbienvenusb@gmail.com>
2022-11-16 10:55:04 +0200

    Design Login,Registration,Password Recovery forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 end
