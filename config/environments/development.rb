fad4285e6b2def9fdca48cb4c68683c0cc9d11bc
usbbush <jeanbienvenusb@gmail.com>
2022-12-29 02:00:47 +0200

    Update deployment configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "recipe-app-production-4574.up.railway.app/"
+  config.hosts << "recipe-app-production-4574.up.railway.app"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
