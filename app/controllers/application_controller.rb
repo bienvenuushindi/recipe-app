5d7636252fc26e3e038a93acff015fdefc9e560b
Ushindi Bienvenu <jeanbienvenusb@gmail.com>
2023-01-04 09:17:45 +0200

    Redirect to public_recipes after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
+  
+  def after_sign_in_path_for(_resource_or_scope)
+    public_recipes_path
+  end
 end
